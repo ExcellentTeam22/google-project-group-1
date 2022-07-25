b2f3de6bacdffcd3d0ba4b7edd53ff70907ee7af
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-07-25 23:14:48 +0300

    add search function and service function and import json and save as dictionary

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ from glob import glob
 import re
 import json
 
+
+def import_dictionary_from_json(path: str) -> dict:
+    with open('file.txt') as json_file:
+        data = json.load(json_file)['words_dic']
+        for key, value in data.items():
+            print(key)
+    return data
+
+def search_word(str_input:str,input_words:dict):
+    input_words = list(filter(None, str_input))
+    if all(str in str_input for name in input_words):
+        first_word = str_input.split()[0]
+
+
+
+def user_service():
+    str_input=input("The system is ready. Enter your text")
+    search_word(str_input)
+
+
+
+
+
 if __name__ == '__main__':
 
     # offline
@@ -18,15 +41,21 @@ if __name__ == '__main__':
             temp_line = re.split('[^a-zA-Z\d]', temp_line)
             temp_line = list(filter(None, temp_line))
             for word_index, word in enumerate(temp_line):
-                print(word)
                 if word in words_dic:
                     words_dic[word] += [(result[index], line_index, word_index)]
                 else:
                     words_dic[word] = [(result[index], line_index, word_index)]
             #print(words_dic["terminal"])
     exDict = {'words_dic':words_dic}
-    with open('file.txt', 'w') as file:
-        file.write(json.dumps(exDict))
+
+    user_service(import_dictionary_from_json('file.txt'))
+
+# with open('file.txt', 'w') as file:
+      #  file.write(json.dumps(exDict))
+
+
+
+
 
 
     # online
